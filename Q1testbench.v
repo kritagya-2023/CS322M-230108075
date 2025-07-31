9967247f2d26a8b0bf416b5fd2fda5368c5e8ddd
kritagya-2023 <kritagya9552@gmail.com>
2025-07-31 09:41:44 +0530

    LAB1 Q1

---
+++ Q1testbench.v
@@ -0,0 +1,18 @@
+module tb_comparator_1bit;
+    reg A, B;
+    wire o1, o2, o3;
+
+    comparator_1bit uut (
+        .A(A), .B(B),
+        .o1(o1), .o2(o2), .o3(o3)
+    );
+
+    initial begin
+        $display("A B | o1 o2 o3");
+        A = 0; B = 0; #10; $display("%b %b |  %b  %b  %b", A, B, o1, o2, o3);
+        A = 0; B = 1; #10; $display("%b %b |  %b  %b  %b", A, B, o1, o2, o3);
+        A = 1; B = 0; #10; $display("%b %b |  %b  %b  %b", A, B, o1, o2, o3);
+        A = 1; B = 1; #10; $display("%b %b |  %b  %b  %b", A, B, o1, o2, o3);
+        $finish;
+    end
+endmodule
